7580d1dd105c6e1eed2b7750e6bf796ea91511bb
Rtiemm <99262095+Rtiemm@users.noreply.github.com>
2022-04-03 18:48:52 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,9 +1,21 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
-template<typename T, int size>
+template <typename type, int size>
 class TStack {
-
+ private:
+  type arr[100];
+  int top;
+ public:
+  TStack() : top(-1) {}
+  type get() const { return arr[top]; }
+  bool isEmpty() const { return top == -1; }
+  bool isFull() const { return top == size - 1; }
+  void pop() {
+    if (top >= 0) top--;
+  }
+  void push(type item) {
+    if (top < size - 1) arr[++top] = item;
+  }
 };
-#endif  // INCLUDE_TSTACK_H_
+#endif
